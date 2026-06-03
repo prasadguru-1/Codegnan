8526bb7772a6de47f177de422d15a4170d27848e
prasadguru-1 <guruprasaddoddipalli1@gmail.com>
2026-06-03 14:40:51 +0530

    Add files via upload

---
+++ variabels.py.py
@@ -0,0 +1,116 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+VARIABLES:print(7+9)
+16
+a=10
+print(a)
+10
+b=30
+print(b)
+30
+5=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+7=100
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a7=100
+print(a7)
+100
+20a=40
+SyntaxError: invalid decimal literal
+a0123456789=30
+print(a0123456789)
+30
+0=10
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+name="guru"
+print(name)
+guru
+print("name")
+name
+place="vij
+SyntaxError: unterminated string literal (detected at line 1)
+place="vij"
+print(place)
+vij
+country="india"
+print(country)
+india
+#=20
+@=90
+SyntaxError: invalid syntax
+$=80
+SyntaxError: invalid syntax
+_=30
+print(_)
+30
+_a=80
+print(_)
+30
+if=30
+SyntaxError: invalid syntax
+else=90
+SyntaxError: invalid syntax
+a=3,b=8
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=3;b=8
+print(a+b)
+11
+a,b=6,7
+print(a+b)
+13
+>>> a,b,c=20
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    a,b,c=20
+TypeError: cannot unpack non-iterable int object
+>>> print(a,b,c)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print(a,b,c)
+NameError: name 'c' is not defined
+>>> a=b=c=50
+>>> print(a,b,c,)
+50 50 50
+>>> a,b,c,=10
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    a,b,c,=10
+TypeError: cannot unpack non-iterable int object
+>>> first name="guru"
+SyntaxError: invalid syntax
+>>> first_name="guru"
+>>> print(first_name)
+guru
+>>> firstname="guru"
+>>> print(firstname)
+guru
+>>> fname="guru"
+>>> lname"md"
+SyntaxError: invalid syntax
+>>> fname="guru"
+>>> lname="md"
+>>> print(fname+lname)
+gurumd
+>>> a,b,c=(6,7,8)
+>>> print(a,b,c)
+6 7 8
+>>> x=10
+>>> print(x)
+10
+>>> dela
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    dela
+NameError: name 'dela' is not defined
+>>> age=10
+>>> print(age)
+10
+>>> AGE=21
+>>> print(AGE)
+21
+>>> age=20
+>>> print=(age)
+
+
+
+
